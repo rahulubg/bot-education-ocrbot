1864d126aabf9917715e4d3cfa0c36cbc66cf52d
rahulubg <Rahulubgade@hotmail.com>
2018-09-16 11:54:29 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 var _config = {
     CHAT_CONNECTOR: {
-        APP_ID: , //You can obtain an APP ID and PASSWORD here: https://dev.botframework.com/bots/new
-        APP_PASSWORD: 
+        APP_ID: process.env.MICROSOFT_APP_ID , //You can obtain an APP ID and PASSWORD here: https://dev.botframework.com/bots/new
+        APP_PASSWORD: process.env.MICROSOFT_APP_PASSWORD
     },
     COMPUTER_VISION_SERVICE: {
         API_URL: "https://eastus.api.cognitive.microsoft.com/vision/v1.0",
